076aca4c92449fbd4127c2fe5a132a72d4304e35
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-09 11:21:05 +0900

    :bug:ログファイルが存在しなかった場合のバリデーションを実装する #24

--- function.sh
+++ function.sh
@@ -28,9 +28,26 @@ function handler() {
 
     to_params "${1}"
 
+    # Download log files to the working directory.
+    # 作業ディレクトリにログファイルをダウンロードする
+    WORK_DIR=".work"
+    if [ -d ${WORK_DIR} ]; then
+        rm -fr ${WORK_DIR}
+    fi
+    mkdir -p ${WORK_DIR}
+
+    cd ${WORK_DIR}
     aws s3 cp s3://"${BUCKET}/${BUCKET_PATH}" . --recursive > /dev/null
     # [Caution] ${BUCKET} is the bucket name in S3. It should be set to an environment variable.
     # 【注意】${BUCKET} はS3 のバケット名。環境変数に設定しておくこと。
+
+    # If the log file does not exist, the process is terminated.
+    # ログファイルが存在しなければ処理を終了する。
+    if [ -z "$(ls .)" ] ; then
+        (cd - && rm -fr ${WORK_DIR}) > /dev/null
+        curl -X POST -H 'Content-type: application/json' -d "{\"text\":\"Log files is not found.\"}" "${SLACK_WEBHOOK_URL}"
+        exit 1;
+    fi
 
     local -r result=$(find . -type f -name "aws-*" \
         | xargs jq "select(.action == \"ALLOW\" and .httpRequest.uri == \"${URI}\") | {clientIp: .httpRequest.clientIp, headers: .httpRequest.headers, coutry: .httpRequest.country}")
@@ -39,6 +56,7 @@ function handler() {
     local -r clientip=$(echo "${result}" | grep "clientIp" | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,$//g' | sort | uniq)
     echo "${clientip}" 1>&2;  # => clientIp
 
+    (cd - && rm -fr ${WORK_DIR}) > /dev/null
     curl -X POST -H 'Content-type: application/json' -d "{\"text\":\"${URI}:${clientip}\"}" "${SLACK_WEBHOOK_URL}"
     # Slack's Incoming Webhook only notifies the client IP.
     # Slack の Incoming Webhook には クライアント IP だけを通知する
